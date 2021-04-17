6d5167352e7d9be130ddc87dce691a3dc7ff109d
Billbern <denis.abaidoo@yahoo.com>
2021-04-17 03:55:10 +0000

    added the interface

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import './components/uploader';
+import Uploader from './components/uploader';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
+        <div>
+          <a href="/">Mong<span>Converter</span></a>
+          <ul>
+            <li>About</li>
+            <li>How It Works</li>
+            <li>FAQ</li>
+          </ul>
+        </div>
       </header>
+      <main>
+        <div className="App_right">
+          <Uploader/>
+          <div className="App_load">
+
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/uploader.js
@@ -0,0 +1,18 @@
+import upLoad from '../upload.svg';
+
+function Uploader() {
+    return(
+        <div>
+            <div className="load_par">
+                <div className="load_inner">
+                    <div className="load_content">
+                        <upLoad />
+                    </div>
+                </div>
+            </div>    
+        </div>
+    );
+}
+
+
+export default Uploader;
